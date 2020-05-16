562ca770c111fecbb5b04b5b901c3d86a0d3215c
____marcell <0000marcell@gmail.com>
2020-05-16 19:32:39 +0000

    commander added

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
-  open = require('open')
+  open = require('open'),
+  program = require('commander');
 
-app.use(express.json())
-  .use(bodyParser.json())
+const runServer = () => {
+  app.use(express.json())
+    .use(bodyParser.json())
 
-app.get('/', function (req, res) {
-  res.send('<p>The frontend will live here!</p>');
-})
+  app.get('/', function (req, res) {
+    res.send('<p>The frontend will live here!</p>');
+  })
 
-app.listen(4200, function () {
-  console.log('fake server running on 4200!');
+  app.listen(4200, function () {
+    console.log('fake server running on 4200!');
+  });
+
+  open('http://localhost:4200/')
+} 
+
+program
+  .description('run server!')
+  .action(async function(cmd) {
+  runServer()  
 });
 
-open('http://localhost:4200/')
+program.parse(process.argv);
+
+
