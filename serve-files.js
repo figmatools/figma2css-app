e029ac30ba90cf69332ecb3d10b59b067c743366
____marcell <0000marcell@gmail.com>
2020-07-28 19:35:23 +0000

    build included

--- serve-files.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var frontend = express();
-
-//setting middleware
-frontend.use(express.static(__dirname + 'public')); //Serves resources from public folder
-
-
-var server = frontend.listen(5000);
